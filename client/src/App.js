6f2c06100bf04ebe91a3c7eec0d5b5dae3067b08
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2019-05-13 18:26:17 +0200

    Fetch GOT characters from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const fetchCharacters = async () => {
+  const query = `query Query { characters { name } }`;
+
+  try {
+    await fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ query })
+    })
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => fetchCharacters()}>
+        Fetch characters
+      </button>
     </div>
   );
 }
