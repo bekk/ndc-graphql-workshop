404f7b1e3f03ebb26ac4810d4734876e16748b93
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2019-05-27 17:24:42 +0200

    List all characters in client

---
+++ client/src/Character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Character extends React.Component {
+    render() {
+
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default withRouter(Character);
