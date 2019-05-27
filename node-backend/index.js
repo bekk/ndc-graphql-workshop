9a29f3f66e91bb64e3a8d581cd959721a7095917
Simen Støa <simen.stoa@bekk.no>
2019-05-27 18:03:43 +0200

    Added data for houses.

--- node-backend/index.js
+++ node-backend/index.js
@@ -12,37 +12,37 @@ const Query = {
 };
 
 const Character = {
-  siblings: (root, args, context) => {
+  siblingIds: (root, args, context) => {
     const siblings = root.related.filter(rel =>
       root.siblings.includes(rel.name)
     );
     return siblings.map(sib => characters.find(c => c.slug === sib.slug)._id);
   },
-  house: (root, args, context) => {
+  houseId: (root, args, context) => {
     const house = houses.find(h => h.name === root.house);
     return house && house._id;
   },
-  allegiances: (root, args, context) => {
-    const allegiances = root.related.filter(rel =>
-      root.allegiances.includes(rel.name)
-    );
-    return allegiances.map(
-      sib => characters.find(c => c.slug === sib.slug)._id
-    );
-  },
-  spouse: (root, args, context) => {
+  spouseIds: (root, args, context) => {
     const spouse = root.related.filter(rel => root.spouse.includes(rel.name));
     return spouse.map(sib => characters.find(c => c.slug === sib.slug)._id);
   },
-  lovers: (root, args, context) => {
+  loverIds: (root, args, context) => {
     const lovers = root.related.filter(rel => root.lovers.includes(rel.name));
     return lovers.map(sib => characters.find(c => c.slug === sib.slug)._id);
   }
 };
 
 const House = {
+  id: (root, args, context) => {
+    return root._id;
+  },
   image: (root, args, context) => {
     return root.logoURL;
+  },
+  allegionHouseIds: (root, args, context) => {
+    return houses
+      .filter(house => root.allegiance.includes(house.name))
+      .map(alg => alg._id);
   }
 };
 
