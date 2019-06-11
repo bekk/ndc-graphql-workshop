f1cb6e28005b3b83ab83bc7dad74703a4949dad8
Simen Støa <simen.stoa@bekk.no>
2019-06-11 21:36:01 +0200

    Added some comments to node backbone

--- node/index.js
+++ node/index.js
@@ -1,19 +1,27 @@
 const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = require("./schema.js");
-const characters = require("./data/characters.js");
 
+// In-memory data storage
+const characters = require("./data/characters.js");
+const houses = require("./data/houses.js");
+const castles = require("./data/castles.js");
+
+// This is the Query resolver
 const Query = {
   characters: (root, args, context) => {
     return characters;
   }
 };
 
+// This is the Character resolver
+// Scalar fields with the same name as in the js object are resolved automatically.
 const Character = {
   siblings: (root, args, context) => {
     return characters.filter(char => root.siblingIds.includes(char.id));
   }
 };
 
+// All resolvers must be added to the resolver object
 const resolvers = {
   Query,
   Character
