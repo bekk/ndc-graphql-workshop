9dd90ea69c843823df1a79cf29807cd990c97c43
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2019-06-12 18:38:35 +0200

    Add some styling. Reload site on changes

--- client/src/Character.js
+++ client/src/Character.js
@@ -6,7 +6,16 @@ import { ADD_CHARACTER_TITLE, GET_CHARACTER, PUSH_CHARACTER } from './queries';
 const Push = ({ name }) => {
   return (
     <Mutation mutation={PUSH_CHARACTER}>
-      {pushFromWindow => <button onClick={() => pushFromWindow({ variables: { name } })}>Push</button>}
+      {pushFromWindow => (
+        <button
+          onClick={() => {
+            pushFromWindow({ variables: { name } });
+            window.location.reload();
+          }}
+        >
+          Push
+        </button>
+      )}
     </Mutation>
   );
 };
@@ -18,15 +27,11 @@ const AddTitle = ({ name }) => {
     <Mutation mutation={ADD_CHARACTER_TITLE}>
       {addTitle => (
         <div>
-          <input
-            type="text"
-            value={titleInput}
-            onChange={event => setTitleInput(event.currentTarget.value)}
-          />
+          <input type="text" value={titleInput} onChange={event => setTitleInput(event.currentTarget.value)} />
           <button
             onClick={() => {
               addTitle({ variables: { name, title: titleInput } });
-              setTitleInput('');
+              window.location.reload();
             }}
           >
             Add title
@@ -41,17 +46,26 @@ const View = ({ character }) => {
   const { name, image, titles, isHealthy } = character;
 
   return (
-    <section>
+    <section className="character">
       <h1>{name}</h1>
-      <p>{`isHealthy: ${isHealthy}`}</p>
-      <ul>
-        {titles.map(title => (
-          <li>{title}</li>
-        ))}
-      </ul>
-      <img src={image} alt="" />
-      <AddTitle name={name} />
-      <Push name={name} />
+      <div className="character-info">
+        <div className="character-image">
+          <img src={image} alt="" className="image-large" />
+          {!isHealthy && <div className="hurt">IS HURTING</div>}
+        </div>
+        <div className="characteristics">
+          <strong>Titles</strong>
+          <ul>
+            {titles.map(title => (
+              <li>{title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="character-actions">
+        <AddTitle name={name} />
+        <Push name={name} />
+      </div>
     </section>
   );
 };
